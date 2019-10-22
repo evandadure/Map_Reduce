d8cb2b7bb1efb7b2a5f01f95ae05ddd7a7b9abc4
evandadure <evan.dadure@gmail.com>
2019-10-22 17:20:18 +0200

    premier map

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import map
 
 if __name__ == "__main__":
-    map.map("salutt")
+    map.map("salutn a c'Est évan jahaz@salutn @dç sfd!a!quoh dsf !salutn!!:: aa!a")
--- map.py
+++ map.py
@@ -1,5 +1,14 @@
+import re
+
 def map(input_str):
-    print(input_str)
+    res = [word.casefold() for word in re.findall(r'\w+', input_str)]
+    dict={}
+    for word in res:
+        if word in dict:
+            dict[word]+=1
+        else:
+            dict[word]=1
+    return(dict)
 
 
 
