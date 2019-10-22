41e5b4c94e3b749982b5c86bb6e096463b3c61da
Boulix <maxenceperso@gmail.com>
2019-10-22 17:16:13 +0200

    fonction reduce done

---
+++ reduce.py
@@ -0,0 +1,21 @@
+
+d1 = {'a': 2, 'b': 3, 'c': 4}
+d2 = {'d': 2, 'e': 3, 'f': 4}
+d3 = {'g': 2, 'h': 3, 'd': 4}
+d4 = {'a': 2, 'b': 3, 'c': 4}
+
+
+l = [d1, d2, d3, d4]
+
+def reduce(listdict):
+    res = {}
+    for dic in listdict:
+        for key, value in dic.items():
+            if key in res.keys():
+                res[key] = res[key] + value
+            else:
+                res[key] = value
+    return res
+
+
+print(reduce(l))
