bf545157c350ebf9854f147eb9313ca8ec981895
Maxence Personnaz <maxenceperso@gmail.com>
2019-12-04 13:28:36 +0100

    Reducers

--- reduce.py
+++ reduce.py
@@ -1,35 +1,35 @@
 import json
 
 
-d1 = {'a': 2, 'b': 3, 'c': 4}
-d2 = {'d': 2, 'e': 3, 'f': 4}
-d3 = {'g': 2, 'h': 3, 'd': 4}
-d4 = {'a': 2, 'b': 3, 'c': 4}
-
-
-l = [d1, d2, d3, d4]
-
-
 def readMapFile(urls):
+    big_dicList = []
     for url in urls:
-        
-    with open('file.json', 'r') as f:
-        distros_dict = json.load(f)
-        print(distros_dict["fruit"])
+        with open(url, 'r') as f:
+            big_dicList.append(json.load(f))
+    return big_dicList        
     
     
-    
-def reduce(listdict):
+def reduce(big_dicList, index):
     res = {}
-    for dic in listdict:
-        for key, value in dic.items():
+    for big_dic in big_dicList:
+        small_dic = big_dic[str(index)]
+        for key, value in small_dic.items():
             if key in res.keys():
                 res[key] = res[key] + value
             else:
                 res[key] = value
     return res
 
+def writeFileJson(small_dic, index):
+    with open('../data/reduce'+str(index)+'.json', 'w') as reduceJson:
+        json.dump(small_dic, reduceJson)
 
-readMapFile(1)
+def execReduce(urls, index):
+    big_dicList = readMapFile(urls)
+    small_dic = reduce(big_dicList, index)
+    writeFileJson(small_dic, index)
 
+
+urls = ["../data/file.json", "../data/file copy.json"]
+execReduce(urls, 0)
 #print(reduce(l))
